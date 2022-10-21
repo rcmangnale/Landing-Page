1dc4fde0325effa07208fad9cda2f20b47a2f5dd
Rameshwar Mangnale <mangnalercm97@gmail.com>
2022-10-22 00:04:40 +0530

    mordern file update

--- src/components/Mordern.js
+++ src/components/Mordern.js
@@ -1,73 +1,139 @@
-
-export default function Mordern  ()  {
-    return (
-        <div className="feature">
-            <div className="grid grid-cols-1 feature-container lg:grid-cols-3 md:grid-col-2">
-                <div className="feature-box">
-                    <div className="feature-logo">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="29" height="34" viewBox="0 0 29 34">
-                            <g fill="none" fill-rule="evenodd">
-                                <g fill-rule="nonzero">
-                                    <g>
-                                        <g>
-                                            <g>
-                                                <path fill="#5454D4" d="M10.812 18.63H.676c-.373 0-.676-.321-.676-.717V.717C0 .32.303 0 .676 0h10.136c.373 0 .676.321.676.717v17.196c0 .396-.303.717-.676.717z" transform="translate(-222 -947) translate(0 947.7) translate(220.5) translate(1.8)"/>
-                                                <path fill="#D8D7E6" d="M10.812 32.4H.676c-.373 0-.676-.337-.676-.752v-9.026c0-.415.303-.752.676-.752h10.136c.373 0 .676.337.676.752v9.026c0 .415-.303.752-.676.752zM27.224 10.53H17.088c-.373 0-.676-.337-.676-.752V.752c0-.415.303-.752.676-.752h10.136c.373 0 .676.337.676.752v9.026c0 .415-.303.752-.676.752z" transform="translate(-222 -947) translate(0 947.7) translate(220.5) translate(1.8)"/>
-                                                <path fill="#5454D4" d="M27.224 32.4H17.088c-.373 0-.676-.321-.676-.717V14.487c0-.396.303-.717.676-.717h10.136c.373 0 .676.321.676.717v17.196c0 .396-.303.717-.676.717z" transform="translate(-222 -947) translate(0 947.7) translate(220.5) translate(1.8)"/>
-                                            </g>
-                                        </g>
-                                    </g>
-                                </g>
-                            </g>
-                        </svg>
-                    </div>
-                    <h1 className="feature-title">Multiple Modern Layouts</h1>
-                    <p className="feature-text">With lots of unique blocks, you can easily build a page without coding. Build your next landing page quickly.</p>
-                </div>
-                <div className="feature-box">
-                    <div className="feature-logo">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" viewBox="0 0 35 35">
-                            <g fill="none" fill-rule="evenodd">
-                                <g fill-rule="nonzero">
-                                    <g>
-                                        <g>
-                                            <g>
-                                                <path fill="#5454D4" d="M32.925 22.611L29.64 20.7l-11.86 6.897c-.698.407-1.556.407-2.253 0L3.669 20.7.38 22.611c-.236.138-.381.393-.381.67 0 .276.145.531.381.668l15.894 9.25c.232.135.518.135.75 0l15.894-9.25c.236-.137.381-.392.381-.669 0-.276-.145-.531-.381-.669h.006z" transform="translate(-563 -947) translate(0 947.7) translate(562.5) translate(.9)"/>
-                                                <path fill="#D8D7E6" d="M32.925 16.311L29.64 14.4l-11.86 6.897c-.698.407-1.556.407-2.253 0L3.669 14.4.38 16.311c-.236.138-.381.393-.381.67 0 .276.145.531.381.668l15.894 9.25c.232.135.518.135.75 0l15.894-9.25c.236-.137.381-.392.381-.669 0-.276-.145-.531-.381-.669h.006z" transform="translate(-563 -947) translate(0 947.7) translate(562.5) translate(.9)"/>
-                                                <path fill="#5454D4" d="M32.919 9.239L17.025.1c-.232-.133-.518-.133-.75 0L.38 9.24C.145 9.375 0 9.627 0 9.9s.145.525.381.661L16.275 19.7c.232.133.518.133.75 0L32.92 10.56c.236-.136.381-.388.381-.661s-.145-.525-.381-.661z" transform="translate(-563 -947) translate(0 947.7) translate(562.5) translate(.9)"/>
-                                            </g>
-                                        </g>
-                                    </g>
-                                </g>
-                            </g>
-                        </svg>
-                    </div>
-                    <h1 className="feature-title">Multiple Modern Layouts</h1>
-                    <p className="feature-text">With lots of unique blocks, you can easily build a page without coding. Build your next landing page quickly.</p>
-                </div>
-                <div className="feature-box">
-                    <div className="feature-logo">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="34" height="34" viewBox="0 0 34 34">
-                            <g fill="none" fill-rule="evenodd">
-                                <g fill-rule="nonzero">
-                                    <g>
-                                        <g>
-                                            <g>
-                                                <path fill="#D8D7E6" d="M1.421 6.794H13.5V5.4H.71c-.392 0-.71.311-.71.697v20.206c0 .386.318.697.71.697h2.132v-1.394h-1.42V6.794z" transform="translate(-906 -947) translate(0 947.7) translate(904.5) translate(1.8)"/>
-                                                <path fill="#5454D4" d="M12.808 19.8H5.192c-.383 0-.692.313-.692.7v11.2c0 .387.31.7.692.7h7.616c.383 0 .692-.313.692-.7V20.5c0-.387-.31-.7-.692-.7zM9 30.3c-.573 0-1.038-.47-1.038-1.05 0-.58.465-1.05 1.038-1.05s1.038.47 1.038 1.05c0 .58-.465 1.05-1.038 1.05zM26.479 0H15.82C15.037 0 14.4.624 14.4 1.39v12.52c0 .766.637 1.39 1.421 1.39H26.48c.784 0 1.421-.624 1.421-1.39V1.39C27.9.625 27.263 0 26.479 0zM21.15 13.214c-.588 0-1.066-.468-1.066-1.044 0-.575.478-1.043 1.066-1.043.588 0 1.066.468 1.066 1.043 0 .576-.478 1.044-1.066 1.044z" transform="translate(-906 -947) translate(0 947.7) translate(904.5) translate(1.8)"/>
-                                                <path fill="#D8D7E6" d="M31.68 5.4h-2.16v1.4h1.44v18.9H14.4v4.9h7.2c.397 0 .72-.314.72-.7 0-.386-.323-.7-.72-.7h-2.88v-2.1h12.96c.398 0 .72-.313.72-.7V6.1c0-.387-.322-.7-.72-.7z" transform="translate(-906 -947) translate(0 947.7) translate(904.5) translate(1.8)"/>
-                                            </g>
-                                        </g>
-                                    </g>
-                                </g>
-                            </g>
-                        </svg>
-                    </div>
-                    <h1 className="feature-title">Multiple Modern Layouts</h1>
-                    <p className="feature-text">With lots of unique blocks, you can easily build a page without coding. Build your next landing page quickly.</p>
-                </div>
-            </div>
+export default function Mordern() {
+  return (
+    <div className=" feature">
+      <div className="container mx-auto mt-4 max-w-7xl">
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3">
+          <div className="max-w-sm p-6 bg-white ">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="29"
+              height="34"
+              viewBox="0 0 29 34"
+            >
+              <g fill="none" fill-rule="evenodd">
+                <g fill-rule="nonzero">
+                  <g>
+                    <g>
+                      <g>
+                        <path
+                          fill="#5454D4"
+                          d="M10.812 18.63H.676c-.373 0-.676-.321-.676-.717V.717C0 .32.303 0 .676 0h10.136c.373 0 .676.321.676.717v17.196c0 .396-.303.717-.676.717z"
+                          transform="translate(-222 -947) translate(0 947.7) translate(220.5) translate(1.8)"
+                        />
+                        <path
+                          fill="#D8D7E6"
+                          d="M10.812 32.4H.676c-.373 0-.676-.337-.676-.752v-9.026c0-.415.303-.752.676-.752h10.136c.373 0 .676.337.676.752v9.026c0 .415-.303.752-.676.752zM27.224 10.53H17.088c-.373 0-.676-.337-.676-.752V.752c0-.415.303-.752.676-.752h10.136c.373 0 .676.337.676.752v9.026c0 .415-.303.752-.676.752z"
+                          transform="translate(-222 -947) translate(0 947.7) translate(220.5) translate(1.8)"
+                        />
+                        <path
+                          fill="#5454D4"
+                          d="M27.224 32.4H17.088c-.373 0-.676-.321-.676-.717V14.487c0-.396.303-.717.676-.717h10.136c.373 0 .676.321.676.717v17.196c0 .396-.303.717-.676.717z"
+                          transform="translate(-222 -947) translate(0 947.7) translate(220.5) translate(1.8)"
+                        />
+                      </g>
+                    </g>
+                  </g>
+                </g>
+              </g>
+            </svg>
+            <a href="#">
+              <h5 className="mb-2 text-2xl font-semibold tracking-tight text-black">
+                Multiple Modern Layouts
+              </h5>
+            </a>
+            <p className="mb-3 font-normal text-gray-500 dark:text-gray-400">
+              With lots of unique blocks, you can easily build a page without
+              coding. Build your next landing page quickly.
+            </p>
+          </div>
+          <div className="max-w-sm p-6 bg-white ">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="35"
+              height="35"
+              viewBox="0 0 35 35"
+            >
+              <g fill="none" fill-rule="evenodd">
+                <g fill-rule="nonzero">
+                  <g>
+                    <g>
+                      <g>
+                        <path
+                          fill="#5454D4"
+                          d="M32.925 22.611L29.64 20.7l-11.86 6.897c-.698.407-1.556.407-2.253 0L3.669 20.7.38 22.611c-.236.138-.381.393-.381.67 0 .276.145.531.381.668l15.894 9.25c.232.135.518.135.75 0l15.894-9.25c.236-.137.381-.392.381-.669 0-.276-.145-.531-.381-.669h.006z"
+                          transform="translate(-563 -947) translate(0 947.7) translate(562.5) translate(.9)"
+                        />
+                        <path
+                          fill="#D8D7E6"
+                          d="M32.925 16.311L29.64 14.4l-11.86 6.897c-.698.407-1.556.407-2.253 0L3.669 14.4.38 16.311c-.236.138-.381.393-.381.67 0 .276.145.531.381.668l15.894 9.25c.232.135.518.135.75 0l15.894-9.25c.236-.137.381-.392.381-.669 0-.276-.145-.531-.381-.669h.006z"
+                          transform="translate(-563 -947) translate(0 947.7) translate(562.5) translate(.9)"
+                        />
+                        <path
+                          fill="#5454D4"
+                          d="M32.919 9.239L17.025.1c-.232-.133-.518-.133-.75 0L.38 9.24C.145 9.375 0 9.627 0 9.9s.145.525.381.661L16.275 19.7c.232.133.518.133.75 0L32.92 10.56c.236-.136.381-.388.381-.661s-.145-.525-.381-.661z"
+                          transform="translate(-563 -947) translate(0 947.7) translate(562.5) translate(.9)"
+                        />
+                      </g>
+                    </g>
+                  </g>
+                </g>
+              </g>
+            </svg>
+            <a href="#">
+              <h5 className="mb-2 text-2xl font-semibold tracking-tight text-black">
+                Multiple Modern Layouts
+              </h5>
+            </a>
+            <p className="mb-3 font-normal text-gray-500 dark:text-gray-400">
+              With lots of unique blocks, you can easily build a page without
+              coding. Build your next landing page quickly.
+            </p>
+          </div>
+          <div className="max-w-sm p-6 bg-white ">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="34"
+              height="34"
+              viewBox="0 0 34 34"
+            >
+              <g fill="none" fill-rule="evenodd">
+                <g fill-rule="nonzero">
+                  <g>
+                    <g>
+                      <g>
+                        <path
+                          fill="#D8D7E6"
+                          d="M1.421 6.794H13.5V5.4H.71c-.392 0-.71.311-.71.697v20.206c0 .386.318.697.71.697h2.132v-1.394h-1.42V6.794z"
+                          transform="translate(-906 -947) translate(0 947.7) translate(904.5) translate(1.8)"
+                        />
+                        <path
+                          fill="#5454D4"
+                          d="M12.808 19.8H5.192c-.383 0-.692.313-.692.7v11.2c0 .387.31.7.692.7h7.616c.383 0 .692-.313.692-.7V20.5c0-.387-.31-.7-.692-.7zM9 30.3c-.573 0-1.038-.47-1.038-1.05 0-.58.465-1.05 1.038-1.05s1.038.47 1.038 1.05c0 .58-.465 1.05-1.038 1.05zM26.479 0H15.82C15.037 0 14.4.624 14.4 1.39v12.52c0 .766.637 1.39 1.421 1.39H26.48c.784 0 1.421-.624 1.421-1.39V1.39C27.9.625 27.263 0 26.479 0zM21.15 13.214c-.588 0-1.066-.468-1.066-1.044 0-.575.478-1.043 1.066-1.043.588 0 1.066.468 1.066 1.043 0 .576-.478 1.044-1.066 1.044z"
+                          transform="translate(-906 -947) translate(0 947.7) translate(904.5) translate(1.8)"
+                        />
+                        <path
+                          fill="#D8D7E6"
+                          d="M31.68 5.4h-2.16v1.4h1.44v18.9H14.4v4.9h7.2c.397 0 .72-.314.72-.7 0-.386-.323-.7-.72-.7h-2.88v-2.1h12.96c.398 0 .72-.313.72-.7V6.1c0-.387-.322-.7-.72-.7z"
+                          transform="translate(-906 -947) translate(0 947.7) translate(904.5) translate(1.8)"
+                        />
+                      </g>
+                    </g>
+                  </g>
+                </g>
+              </g>
+            </svg>
+            <a href="#">
+              <h5 className="mb-2 text-2xl font-semibold tracking-tight text-black">
+                Multiple Modern Layouts
+              </h5>
+            </a>
+            <p className="mb-3 font-normal text-gray-500 dark:text-gray-400">
+              With lots of unique blocks, you can easily build a page without
+              coding. Build your next landing page quickly.
+            </p>
+          </div>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
-
